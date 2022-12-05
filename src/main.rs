3419d5395ff5d96813b4c7d144509354c9678e8c
René Fuhry <rene.fuhry@gmail.com>
2022-12-05 16:15:09 +0000

    Added the framework for the functions
    Still, have no idea how to execute commands.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 /* wanna rewrite my own update script in rust
  * I need to figure out how to execute shell commands in rust
 */
+
+use std::process::Command;
+
+fn update_with_yay() {
+    println!("This will update your aur and pacman pkg's");
+}
+
+fn update_with_flatpak() {
+    println!("This will update your flatpaks");
+}
+
+fn pre_update_backup() {
+    println!("This will backup important files before updating");
+}
+
+fn post_update_backup() {
+    println!("This will backup important files after updating");
+}
+
+fn main() {
+    pre_update_backup();
+    update_with_yay();
+    update_with_flatpak();
+    post_update_backup();
+}
